cdd8643874bfaff2cfc9f3b9fab3a14dcd482051
Jeremy Green <jeremygreen@Jeremys-MacBook-Pro-2.local>
2014-07-12 22:47:55 -0600

    Initial commit

---
+++ page-edit-toolbar.php
@@ -0,0 +1,55 @@
+<?php 
+
+/*
+Plugin Name: Page Edit Toolbar
+Plugin URI: http://www.endocreative.com
+Description: Adds most recently edited pages to the WordPress Toolbar for easy access
+Version: 1.0
+Author: Endo Creative
+Author URI: http://www.endocreative.com
+*/
+
+add_action( 'admin_bar_menu', 'page_admin_bar_function', 999 );
+
+function page_admin_bar_function( $wp_admin_bar ) {
+
+	// parent page
+	$args = array(
+		'id' => 'page_list',
+		'title' => 'Page List',
+		'href' => home_url() . '/wp-admin/edit.php?post_type=page'
+	);
+	$wp_admin_bar->add_node( $args );
+
+	// get list of pages
+	$pages = recently_edited_pages();
+
+	// loop through up to 10 most recently modified pages
+	foreach( $pages as $page ) {
+
+		// add child nodes (pages to edit)
+		$args = array(
+			'id' => 'page_item_' . $page->ID,
+			'title' => $page->post_title,
+			'parent' => 'page_list',
+			'href' => home_url() . '/wp-admin/post.php?post=' . $page->ID . '&action=edit'
+		);
+		$wp_admin_bar->add_node( $args );
+
+	}
+
+}
+
+function recently_edited_pages() {
+
+	$args = array(
+		'number' => 10,
+		'sort_column' => 'post_modified',
+		'sort_order' => 'desc'
+	);
+
+	$pages = get_posts( $args );
+
+	return $pages;
+	
+}
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Page Edit Toolbar ===
+Contributors: endocreative
+Donate link: http://endocreative.com
+Tags: edit page, toolbar, admin bar, list pages, 
+Requires at least: 3.0.1
+Tested up to: 3.6.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a dropdown to the WordPress toolbar of the 10 most recently edited pages. 
+
+== Description ==
+
+Adds a dropdown 'Page List' to the WordPress toolbar of the 10 most recently edited pages. This gives you quick access to a specific page, helpful when populating a site with content.
+
+This plugin is designed to speed up the process of editing and laying out page content before launch. 
+
+Currently it only displays pages, not posts or custom post types.
+
+== Installation ===
+
+1. Upload `page-edit-toolbar.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. A dropdown will appear in the WordPress toolbar titled 'Page List'.
+
+== Screenshots ==
+
+1. The dropdown shows the 10 most recently modified pages.
+
+== Changelog ==
+
+= 0.1 =
+* Initial plugin submission
